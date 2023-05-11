7d26eccea98680f377577f71f047e15541acda45
Estiawan Rifat <setiawanrifatt@gmail.com>
2023-05-11 14:59:20 +0700

    uts web rifat

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -23,15 +23,33 @@ class Welcome extends CI_Controller
      */
     public function index()
     {
-        // INI KOMENTAR
         $blade = new Blade(VIEWPATH, APPPATH . 'cache');
         echo $blade->make('form', [])->render();
     }
 
     public function tampil()
     {
-        $nama = 'tata';
+
+        if($this->input->post()) {
+            $name = $this->input->post('nama');
+            $nim = $this->input->post('nim');
+            $umur = $this->input->post('umur');
+
+            if($umur < 10) {
+                $age = "Anak";
+            } else if ($umur < 20 && $umur > 10) {
+                $age = "Remaja";
+            } else if($umur > 20 && $umur < 30) {
+                $age = "Dewasa";
+            } else {
+                $age = "Tua";
+            }
+        }
+
+        $nama = $name;
+        $nim = $nim;
+        $umur = $age;
         $blade = new Blade(VIEWPATH, APPPATH . 'cache');
-        echo $blade->make('tampil', ['nama' => $nama])->render();
+        echo $blade->make('tampil', ['nama' => $nama, 'nim' => $nim, 'umur' => $umur])->render();
     }
 }
